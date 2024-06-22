2e9cbfc0af690e3432bf56d3821f4b00b8119c1a
Adriano Simões <adriano.laino@gmail.com>
2024-06-22 01:40:22 -0300

    Add files via upload

---
+++ assets/js/index.js
@@ -0,0 +1,11 @@
+if (localStorage.getItem("token") == null) {
+  alert("Você precisa estar logado para acessar essa página");
+  window.location.href = "./assets/html/singin.html";
+}
+
+
+function sair() {
+  localStorage.removeItem("token");
+  localStorage.removeItem("userLogado");
+  window.location.href = "../html/signin.html";
+}
